1107d14c699a148aa646e59592755a8e092a7c09
Григорий <horoshilovg@gmail.com>
2020-08-20 16:50:16 +0300

    test first2

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ $token = getenv('TELEGRAM_BOT_TOKEN');
 $telegram = new Api($token);
 
 $result = $telegram->getWebhookUpdates();
-$telegram->sendMessage(['chat_id' => $result['message']["chat"]["id"],
-                        'text' => 'hello']);
+
 
 $intents = [];
 $intents[] = new \App\intents\UnknownIntent($telegram, $result);

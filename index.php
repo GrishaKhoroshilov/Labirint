67ed2123ab0534d85d9bba430b1addf9b0cc3db6
Григорий <horoshilovg@gmail.com>
2020-08-20 15:53:52 +0300

    test first

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo '1232';
+
+include('vendor/autoload.php');
+
+use Telegram\Bot\Api;
+
 $token = getenv('TELEGRAM_BOT_TOKEN');
+$telegram = new Api($token);
+
+$result = $telegram->getWebhookUpdates();
+$telegram->sendMessage(['chat_id' => $result['message']["chat"]["id"],
+                        'text' => 'hello']);
+
